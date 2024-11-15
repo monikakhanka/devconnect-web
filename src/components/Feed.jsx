d7374fb8764dd54e7ec76f45719f6894b40f705d
monika <monikakhanka@gmail.com>
2024-11-15 19:29:57 +0530

    interested/ignored request

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {BASE_URL} from "../utils/constants";
+import { BASE_URL } from "../utils/constants";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addFeed } from "../utils/feedSlice";
@@ -7,30 +7,41 @@ import UserCard from "./UserCard";
 
 const Feed = () => {
   const dispatch = useDispatch();
-  const feed = useSelector((store)=> store.feed);
- console.log(feed);
+  const feed = useSelector((store) => store.feed);
+  console.log(feed);
 
-  const getFeed = async ()=>{
-    if(feed) return;
-    
-    try{
-      const res = await axios.get(BASE_URL + "/user/feed", {withCredentials: true});
-      dispatch(addFeed(res.data));
-    }catch(err){
-      console.log("Error: "+ err);
+  const getFeed = async () => {
+    if (feed) return;
+
+    try {
+      const res = await axios.get(BASE_URL + "/user/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res?.data?.data));
+    } catch (err) {
+      console.log("Error: " + err);
     }
-    
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getFeed();
-  },[]);
+  }, []);
 
+  if (!feed) return;
+
+  if (feed.length <= 0)
+    return (
+      <h1 className="flex justify-center my-10 font-bold text-2xl">
+        No new users found!{" "}
+      </h1>
+    );
   return (
-  feed && <div className="flex justify-center my-10">
-    <UserCard user={feed.data[0]}/>
-  </div>
-  )
-}
+    feed && (
+      <div className="flex justify-center my-10">
+        <UserCard user={feed[0]} />
+      </div>
+    )
+  );
+};
 
 export default Feed;
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,27 +1,48 @@
 /* eslint-disable react/prop-types */
 
-const UserCard = ({user}) => {
-const {firstName, lastName, photoUrl, age, gender, about} = user;
+import { useDispatch } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { removeUserFromFeed } from "../utils/feedSlice";
+
+const UserCard = ({ user }) => {
+  const { _id, firstName, lastName, photoUrl, age, gender, about } = user;
+  const dispatch = useDispatch();
+  const handleRequest = async (status, userId) => {
+    const res = await axios.post(
+      BASE_URL + "/request/send/" + status + "/" + userId,
+      {},
+      { withCredentials: true }
+    );
+    dispatch(removeUserFromFeed(userId));
+  };
+
   return (
     <div className="card bg-base-300 w-96 shadow-xl">
-       <div className="justify-center flex">
-            <figure className="mt-5 px-10">
-                    <img
-                    src={photoUrl}
-                    alt="user photo" />
-            </figure>
-       </div>
-        <div className="card-body px-10">
-            <h2 className="card-title">{firstName + " "+ lastName}</h2>
-            {age && gender &&  <p>{age + ", "+ gender}</p>}
-            {about &&<p>{about}</p>}
-            <div className="card-actions justify-center my-4">
-                <button className="btn btn-primary">Ignore</button> 
-                <button className="btn btn-secondary">Ineterested</button>
-            </div>
+      <div className="justify-center flex">
+        <figure className="mt-5 px-10">
+          <img src={photoUrl} alt="user photo" />
+        </figure>
+      </div>
+      <div className="card-body px-10">
+        <h2 className="card-title">{firstName + " " + lastName}</h2>
+        {age && gender && <p>{age + ", " + gender}</p>}
+        {about && <p>{about}</p>}
+        <div className="card-actions justify-center my-4">
+          <button
+            className="btn btn-primary"
+            onClick={() => handleRequest("ignored", _id)}>
+            Ignore
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => handleRequest("interested", _id)}>
+            Interested
+          </button>
         </div>
+      </div>
     </div>
   );
-}
+};
 
-export default UserCard
+export default UserCard;
